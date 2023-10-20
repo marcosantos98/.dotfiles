544b4d25c5f768e912219b5470d681dad75635cc
marcosantos98 <marcomsantos98@gmail.com>
2023-10-20 18:23:51 +0100

    Update dotfiles

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,9 @@
+return {
+	'ollykel/v-vim',
+	{
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+}
+}
