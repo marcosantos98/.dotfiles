4075a5dd2a1bdc423f3428dadc8c11c0ea6cf2ce
marcosantos98 <marcomsantos98@gmail.com>
2023-11-04 18:46:26 +0000

    Add codeextinstall.sh

---
+++ codeextinstall.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+source ../../lib.sh
+
+code --install-extension vscjava.vscode-java-pack
+code --install-extension amatilda.vscode-fasm
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension Equinusocio.vsc-material-theme-icons
+code --install-extension tomphilbin.gruvbox-themes
+code --install-extension Gruntfuggly.todo-tree
