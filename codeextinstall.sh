5b47e9e4c5d9e8515b4cc60057499214513eb089
marcosantos98 <marcomsantos98@gmail.com>
2023-11-10 14:34:01 +0000

    Update codeextinstall.sh

--- codeextinstall.sh
+++ codeextinstall.sh
@@ -6,4 +6,5 @@ code --install-extension ms-vscode.cpptools-extension-pack
 code --install-extension Equinusocio.vsc-material-theme-icons
 code --install-extension tomphilbin.gruvbox-themes
 code --install-extension Gruntfuggly.todo-tree
-code --install-extension emeraldwalk.RunOnSave 
+code --install-extension emeraldwalk.RunOnSave
+code --install-extension vlanguage.vscode-vlang
