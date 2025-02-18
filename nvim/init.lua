206d9d6eae3d3142fb26934f639ae7184b8dcfaf
marcosantos98 <marcomsantos98@gmail.com>
2025-02-18 19:13:37 +0000

    Update nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,3 +5,10 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 999
 vim.o.wrap = false 
+
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+    pattern = "*.lang",
+    callback = function()
+        vim.bo.filetype = "odin"
+    end,
+})
