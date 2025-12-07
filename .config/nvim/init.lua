b237f5f95224bd71170202ce928b571104675535
marcosantos98 <marcomsantos98@gmail.com>
2025-12-07 20:25:51 +0000

    [NVIM] more plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,22 +4,22 @@ vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 999
-vim.o.wrap = false 
+vim.o.wrap = false
 
 vim.o.updatetime = 250
 
 vim.api.nvim_create_autocmd("CursorHold", {
-    callback = function()
-	vim.diagnostic.open_float(nil, {
-	    focusable = false,
-	    close_events = {"BufLeave", "CursorMoved", "InsertEnter", "FocusLost"},
-	    scope = "cursor",
-	    source = "always",
-	    prefix = "",
-	    border = "rounded",
-	})
-    end,
+	callback = function()
+		vim.diagnostic.open_float(nil, {
+			focusable = false,
+			close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+			scope = "cursor",
+			source = "always",
+			prefix = "",
+			border = "rounded",
+		})
+	end,
 })
 
 vim.keymap.set("v", "<C-S-C>", '"+y', { noremap = true, silent = true })
-
+vim.cmd.colorscheme("github-monochrome-dark")
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -22,6 +22,35 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
 	spec = {
 		{
+			"ej-shafran/compile-mode.nvim",
+			version = "^5.0.0",
+			-- you can just use the latest version:
+			-- branch = "latest",
+			-- or the most up-to-date updates:
+			-- branch = "nightly",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+				-- if you want to enable coloring of ANSI escape codes in
+				-- compilation output, add:
+				-- { "m00qek/baleia.nvim", tag = "v1.3.0" },
+			},
+			config = function()
+				---@type CompileModeOpts
+				vim.g.compile_mode = {
+					-- if you use something like `nvim-cmp` or `blink.cmp` for completion,
+					-- set this to fix tab completion in command mode:
+					-- input_word_completion = true,
+
+					-- to add ANSI escape code support, add:
+					-- baleia_setup = true,
+
+					-- to make `:Compile` replace special characters (e.g. `%`) in
+					-- the command (and behave more like `:!`), add:
+					-- bang_expansion = true,
+				}
+			end
+		},
+		{
 			"zaldih/themery.nvim",
 			lazy = false,
 			config = function()
@@ -31,6 +60,9 @@ require("lazy").setup({
 						"github_dark",
 						"tokyonight",
 						"rose-pine",
+						"github-monochrome-dark",
+						"github-monochrome-rosepine",
+						"github-monochrome-tokyonight",
 					},
 					livePreview = true,
 				})
@@ -223,6 +255,12 @@ require("lazy").setup({
 			}
 		},
 		-- :theme
+		{
+			"idr4n/github-monochrome.nvim",
+			lazy = false,
+			priority = 1000,
+			opts = {},
+		},
 		{
 			'projekt0n/github-nvim-theme',
 			config = function()
